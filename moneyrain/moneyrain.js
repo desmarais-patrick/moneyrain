cd0d22f92f52ba27753bc2063bb713a3c0e01f7c
Patrick Desmarais <patrick.desm@gmail.com>
2015-12-06 17:05:14 -0500

    reviewed formatting ot amount to use isNaN, added insertion from client-side of new transactions using the form's submit event

--- moneyrain/moneyrain.js
+++ moneyrain/moneyrain.js
@@ -9,16 +9,44 @@ if (Meteor.isClient) {
 
   Template.transaction.helpers({
     amount: function () {
-      var amount;
-      try {
-        amount = parseFloat(this.value);
-        amount = amount.toFixed(2) + " $";
-      } catch (e) {
+      var amount = parseFloat(this.value);
+
+      if (isNaN(amount)) {
         console.log("Template.transaction.amount",
-          "could not parse this.value as float:", this.value, e);
+          "could not parse this.value as float:", this.value);
         amount = "?";
+      } else {
+        amount = amount.toFixed(2) + " $";
       }
+
       return amount;
+    }
+  });
+
+  Template.body.events({
+    "submit .new-transaction": function (event) {
+      event.preventDefault(); // prevent page reload.
+
+      // Parse values.
+      var description = event.target.description.value;
+      var formValue = event.target.amount.value;
+      var value = parseFloat(formValue);
+
+      if (isNaN(value)) {
+        console.log("Template.events.submit .new-transactions",
+          "could not parse 'value' as float:", formValue);
+        value = formValue;
+      }
+
+      // Add new document.
+      Transactions.insert({
+        description: description,
+        value: value,
+        category: value > 0 ? "revenue": "expense",
+        createdAt: new Date()
+      });
+
+      event.target.reset.click(); // clear form.
     }
   });
 }
