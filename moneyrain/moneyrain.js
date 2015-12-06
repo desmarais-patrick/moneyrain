2cae3a23a87f7ba96069aec446c4ca6db1abad4f
Patrick Desmarais <patrick.desm@gmail.com>
2015-12-05 20:04:20 -0500

    bind to transactions collection

--- moneyrain/moneyrain.js
+++ moneyrain/moneyrain.js
@@ -1,11 +1,10 @@
+Transactions = new Mongo.Collection("transactions");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    transactions: [
-      { description: "Grocery", value: -93.66, category: "expense" },
-      { description: "Lunch", value: -18.54, category: "expense" },
-      { description: "Garage", value: -220.14, category: "expense" },
-      { description: "Gift", value: 20, category: "revenue" }
-    ]
+    transactions: function () {
+      return Transactions.find({});
+    }
   });
 }
 
